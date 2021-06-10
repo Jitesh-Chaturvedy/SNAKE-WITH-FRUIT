2a23391f67c2453ac3183016885106a9a73436fe
Jitesh Chaturvedy <jitesh.chaturvedy@outlook.com>
2021-06-10 23:23:02 +0530

    ARRANGED THE FILES PROFESSIONALLY

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# SNAKE WITH FRUIT:
+SNAKE WITH FRUIT Game is written in Python using the pygame module
+
+# Pygame Module: 
+Pygame Module in python is used to make simple 2D Games.
+
+# Requirement:
+The Game requires the pygame module to be installed in system and offcourse Python too
--- SnakesGUI.py
+++ SnakesGUI.py
@@ -5,20 +5,18 @@ pygame.mixer.init()
 pygame.init()
 
 # Initializing Display Screen
-Screen_Width = 900
-Screen_Height = 600
-Screen = pygame.display.set_mode((Screen_Width, Screen_Height))
+SW = 900
+SH = 600
+Screen = pygame.display.set_mode((SW, SH))
 pygame.display.set_caption("SNAKE AND FRUIT")
 pygame.display.update()
 Clock = pygame.time.Clock()
-SnakeHead = pygame.image.load("SnakeHeadRight.png").convert_alpha()
-Body = pygame.image.load("BodyHor.png").convert_alpha()
-Tail = pygame.image.load("TailRight.png").convert_alpha()
-Food = pygame.image.load("Apple.png").convert_alpha()
+SnakeHead = pygame.image.load("Contents\\SnakeHeadRight.png").convert_alpha()
+Food = pygame.image.load("Contents\\Apple.png").convert_alpha()
 # Global Variable
 Game_Exit = False
 # Black = (0, 0, 0)
-Red = (255, 0, 0)
+# Red = (255, 0, 0)
 
 class ScreenTemplate:
 
@@ -42,30 +40,35 @@ def Home_Screen(): # Completed! Pic editing required
     global Game_Exit
     global SnakeHead
     global Clock
-    Plane = pygame.image.load("HomeScreen.png")
-    SnakeLeft = pygame.image.load("HomeSnakeLeft.png")
-    SnakeRight = pygame.image.load("HomeSnakeRight.png")
-    SnakeUP = pygame.image.load("HomeSnakeUP.png")
-    SnakeDown = pygame.image.load("HomeSnakeDown.png")
+    Red = (255, 0, 0)
+    Plane = pygame.image.load("Backgrounds\\HomeScreen.png")
+    SnakeLeft = pygame.image.load("Contents\\HomeSnakeLeft.png")
+    SnakeRight = pygame.image.load("Contents\\HomeSnakeRight.png")
+    SnakeUP = pygame.image.load("Contents\\HomeSnakeUP.png")
+    SnakeDown = pygame.image.load("Contents\\HomeSnakeDown.png")
     Snake1 = SnakeRight
     Snake2 = SnakeLeft
     Snake_UX = -82
-    Snake_LX = Screen_Width
+    Snake_LX = SW
     Snake_UY = 0
-    Snake_LY = Screen_Height-82
-    HomeScreen = ScreenTemplate(Screen, "HomeScreen.png", "HomeMusic.mp3", 0.4 , 0,0)
+    Snake_LY = SH-82
+    HomeScreen = ScreenTemplate(Screen, "Backgrounds\\HomeScreen.png", "Sounds\\HomeMusic.mp3", 0.4 , 0,0)
     # Home Screen Decoration
     while not Game_Exit:
         Screen.blit(Plane, (0,0))
+        MPos = pygame.mouse.get_pos()
+        MX = MPos[0]
+        MY = MPos[1]
+        pygame.draw.rect(Screen, Red, (SW/8, SH/2.4, 675,65), 2)
         Screen.blit(Snake1, (Snake_UX,Snake_UY))
         Screen.blit(Snake2, (Snake_LX, Snake_LY))
         Snake_UX += 10
         Snake_LX -= 10
-        if Snake_UX>Screen_Width-82:
-            Snake_UX = Screen_Width-82
+        if Snake_UX>SW-82:
+            Snake_UX = SW-82
             Snake1 = SnakeDown
             Snake_UY += 10
-            if Snake_UY>Screen_Height:
+            if Snake_UY>SH:
                 Snake_UY = 0
                 Snake1 = SnakeRight
                 Snake_UX = -82
@@ -76,9 +79,9 @@ def Home_Screen(): # Completed! Pic editing required
             Snake2 = SnakeUP
             Snake_LY -= 10
             if  Snake_LY < -82:
-                Snake_LY = Screen_Height-82
+                Snake_LY = SH-82
                 Snake2 = SnakeLeft
-                Snake_LX = Screen_Width
+                Snake_LX = SW
                 Snake_LX -= 10 
         pygame.display.update()  
         Clock.tick(60) 
@@ -96,32 +99,24 @@ def Home_Screen(): # Completed! Pic editing required
 def Game_Screen(): # Condition defining left
     global Game_Exit
     global SnakeHead
-    global Body
-    global Tail
     global Food
-    global Red
+    # global Red
     Score = 0
     SnakeLength = 1
-    Snake_X = Screen_Width/2
-    Snake_Y = Screen_Height/2
-    SnakeList = [[Snake_X,Snake_Y]]
-    # Body_X = Snake_X-20
-    # Body_Y = Snake_Y + 10 
-    # Tail_X = Body_X-41
-    # Tail_Y = Body_Y
+    Snake_X = SW/2
+    Snake_Y = SH/2
+    # SnakeList = [[Snake_X,Snake_Y]]
     # Fixed Velocity of Snake
     Fix_Velocity_X = 10
     Fix_Velocity_Y = 10
     Velocity_X = 0
     Velocity_Y = 0
     # Position of food
-    Food_X = random.randint(26,Screen_Width-76)
-    Food_Y = random.randint(26,Screen_Height-79)
+    Food_X = random.randint(26,SW-76)
+    Food_Y = random.randint(26,SH-79)
     fps = 60
     # Displaying main Game
-    GameScreen = ScreenTemplate(Screen, "Grass.jpg", "Play.mp3", 0.4, 0,0)
-    # Screen.blit(Body, (Body_X, Body_Y))
-    # Screen.blit(Tail, (Tail_X, Tail_Y))
+    GameScreen = ScreenTemplate(Screen, "Backgrounds\\Grass.jpg", "Sounds\\Play.mp3", 0.4, 0,0)
     Screen.blit(SnakeHead, (Snake_X, Snake_Y))
     Screen.blit(Food, (Food_X, Food_Y))
     pygame.display.update()
@@ -133,49 +128,42 @@ def Game_Screen(): # Condition defining left
                 if event.key == pygame.K_UP:
                     Velocity_Y = -Fix_Velocity_Y
                     Velocity_X =0
-                    SnakeHead = pygame.image.load("SnakeHeadUp.png").convert_alpha()
-                    # Tail = pygame.image.load("TailUp.png").convert_alpha()
-                    # Body = pygame.image.load("BodyVer.png").convert_alpha() 
+                    SnakeHead = pygame.image.load("Contents\\SnakeHeadUp.png").convert_alpha()
                 elif event.key == pygame.K_DOWN:
                     Velocity_Y = +Fix_Velocity_Y
                     Velocity_X = 0
-                    # Body = pygame.image.load("BodyVer.png").convert_alpha()
-                    # Tail = pygame.image.load("TailDown.png").convert_alpha() 
-                    SnakeHead = pygame.image.load("SnakeHeadDown.png").convert_alpha()
+                    SnakeHead = pygame.image.load("Contents\\SnakeHeadDown.png").convert_alpha()
                 elif event.key == pygame.K_LEFT:
                     Velocity_X = -Fix_Velocity_X
                     Velocity_Y = 0
-                    # Body = pygame.image.load("BodyHor.png").convert_alpha()
-                    # Tail = pygame.image.load("TailLeft.png").convert_alpha()
-                    SnakeHead = pygame.image.load("SnakeHeadLeft.png").convert_alpha()
+                    SnakeHead = pygame.image.load("Contents\\SnakeHeadLeft.png").convert_alpha()
                 elif event.key == pygame.K_RIGHT:
                     Velocity_X = +Fix_Velocity_X
                     Velocity_Y = 0                
-                    # Body = pygame.image.load("BodyHor.png").convert_alpha()
-                    # Tail = pygame.image.load("TailRight.png").convert_alpha()
-                    SnakeHead = pygame.image.load("SnakeHeadRight.png").convert_alpha()    
+
+                    SnakeHead = pygame.image.load("Contents\\SnakeHeadRight.png").convert_alpha()    
                 elif event.key == pygame.K_ESCAPE:
                     Home_Screen()
         Snake_X += Velocity_X
         Snake_Y += Velocity_Y        
-        Screen.blit(pygame.image.load("Grass.jpg"),(0,0))
+        Screen.blit(pygame.image.load("Backgrounds\\Grass.jpg"),(0,0))
             
         if abs(Snake_X-Food_X)<15 and abs(Snake_Y-Food_Y)<15:
-            Food_X = random.randint(26,Screen_Width-76)
-            Food_Y = random.randint(26,Screen_Height-79)
+            Food_X = random.randint(26,SW-76)
+            Food_Y = random.randint(26,SH-79)
             Score += 10
             SnakeLength +=1
         Screen.blit(Food, (Food_X, Food_Y))            
         Screen.blit(SnakeHead, (Snake_X, Snake_Y))
         pygame.display.update()
         Clock.tick(fps)
-        if Snake_X<25 or Snake_X>(Screen_Width-85) or Snake_Y<25 or Snake_Y>(Screen_Height-85):
+        if Snake_X<25 or Snake_X>(SW-85) or Snake_Y<25 or Snake_Y>(SH-85):
             # Game_Over = True
             End_Screen()
             
 
 def End_Screen(): # Feature addition under progress
-    SettingScreen = ScreenTemplate(Screen, "Grass 1.jpg", "Settings.mp3", 0.4, 0,0)
+    SettingScreen = ScreenTemplate(Screen, "Backgrounds\\Grass.jpg", "Sounds\\Settings.mp3", 0.4, 0,0)
     while not Game_Exit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -199,9 +187,9 @@ def GameEnd(): # Completed! May require feature addition
     global Game_Exit
     Game_Exit = True
 
-Screen.blit(pygame.image.load("Presenter.jpg"), (0,0))
+Screen.blit(pygame.image.load("Backgrounds\\Presenter.jpg"), (0,0))
 pygame.display.update()
-# time.sleep(1)
+pygame.time.delay(1)
 Home_Screen()
 pygame.quit()
 quit()
